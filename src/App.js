8cc16d3ceed6d159d60f4df7329771c3ecf114aa
Cassiopeia316 <dvinagunawan@gmail.com>
2022-05-31 19:30:51 +0700

    reactdeploytest

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ClassCounter from './pages/ClassCounter';
+import FunctionCounter from './pages/FunctionCounter'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <FunctionCounter />
+    </div>
+  );
+}
+
+export default App;
